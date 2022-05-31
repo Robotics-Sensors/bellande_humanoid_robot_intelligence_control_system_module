17e13d8a060b55d3c57b0911afb254e261b55a35
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-05-31 13:32:17 -0400

    fixes

--- op3_kinematics_dynamics/src/link_data.cpp
+++ op3_kinematics_dynamics/src/link_data.cpp
@@ -1,28 +1,26 @@
 /*******************************************************************************
-* Copyright 2017 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2017 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 /* Authors: SCH, Jay Song */
 
 #include "op3_kinematics_dynamics/link_data.h"
 
-namespace robotis_op
-{
+namespace robotis_op {
 
-LinkData::LinkData()
-{
+LinkData::LinkData() {
   name_ = "";
 
   parent_ = -1;
@@ -45,11 +43,10 @@ LinkData::LinkData()
 
   position_ = robotis_framework::getTransitionXYZ(0.0, 0.0, 0.0);
   orientation_ = robotis_framework::convertRPYToRotation(0.0, 0.0, 0.0);
-  transformation_ = robotis_framework::getTransformationXYZRPY(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+  transformation_ =
+      robotis_framework::getTransformationXYZRPY(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 }
 
-LinkData::~LinkData()
-{
-}
+LinkData::~LinkData() {}
 
-}
+} // namespace robotis_op
